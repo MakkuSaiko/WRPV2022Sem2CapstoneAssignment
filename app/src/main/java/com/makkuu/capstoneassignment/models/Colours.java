820e4fc8663ebbc6c3f84a7c12d60310b1b334e2
MakkuSaiko <adriaanbenn0@gmail.com>
2022-10-05 09:54:35 +0200

    AAAAAAAAAAAAAAAAAAAAAA

--- app/src/main/java/com/makkuu/capstoneassignment/models/Colours.java
+++ app/src/main/java/com/makkuu/capstoneassignment/models/Colours.java
@@ -1,6 +1,20 @@
 package com.makkuu.capstoneassignment.models;
 
+import android.graphics.Color;
+
+import java.util.HashMap;
+
 public class Colours
 {
-    
+    static HashMap<String,Color> colours;
+
+    public Colours getInstance()
+    {
+        return this;
+    }
+
+    static Color getColor(String name)
+    {
+
+    }
 }
